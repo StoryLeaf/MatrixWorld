1b8714a5100b8c8b0f153d068bdbecf6934b609b
StoryLeaf <87229990+StoryLeaf@users.noreply.github.com>
2022-06-04 11:18:15 +1000

    Newest...

--- src/TwoDimensionalMatrix.java
+++ src/TwoDimensionalMatrix.java
@@ -13,9 +13,12 @@ public class TwoDimensionalMatrix {
 
         //Function to output the main matrix.
         void OutputMatrix(){
-            int loopCounter = 0;
+            int loopCounter=0;
+            int colCounter=0;
             System.out.println("\nStart.");
             for(int rowIndex=0;rowIndex<rowAmount;rowIndex++){
+                colCounter++;
+                System.out.print(colCounter+". ");
                 for(int colIndex=0;colIndex<colAmount;colIndex++){
                     System.out.print(worldMatrix[rowIndex][colIndex]);
                     loopCounter++;
